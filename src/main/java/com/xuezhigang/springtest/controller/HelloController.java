fc87e4dc2497ea71f9840e31e2c2e4c1cfa551f5
1022 <786427910@qq.com>
2018-09-10 18:18:41 +0800

    增加一个indexofinteresting方法

---
+++ src/main/java/com/xuezhigang/springtest/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.xuezhigang.springtest.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/hello")
+    private String index() {
+        return "Hello";
+    }
+
+    @RequestMapping("/hello/interesting")
+    private String indexoflast() {
+        return "Very interesting!";
+    }
+}
