f8f2dc1e971da92e3a1e780a3c659f50809c6564
1022 <786427910@qq.com>
2018-09-11 18:03:24 +0800

    进行spring boot 的web端 json和filter的校验

--- src/main/java/com/xuezhigang/springtest/controller/HelloController.java
+++ src/main/java/com/xuezhigang/springtest/controller/HelloController.java
@@ -1,20 +1,34 @@
 package com.xuezhigang.springtest.controller;
 
+import com.xuezhigang.springtest.bean.User;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+
+//@RestController
+@Controller
 @EnableAutoConfiguration
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     private String index() {
         return "Hello";
     }
+    @GetMapping("/getUser")
+    @ResponseBody
+    private User getUser(){
+        User user=new User();
+        user.setUsername("小明2");
+        user.setPassword("xxxx");
+        return user;
 
-    @RequestMapping("/hello/interesting")
-    private String indexoflast() {
-        return "Very interesting!";
     }
+
+    @GetMapping("/hello/interesting")
+    private String indexoflast() {
+        return "/getUser";
+    }
+
 }
---
+++ src/test/java/com/xuezhigang/springtest/filter/WebConfiguration.java
@@ -0,0 +1,58 @@
+package com.xuezhigang.springtest.filter;
+
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.logging.LogRecord;
+
+@Configuration
+public class WebConfiguration {
+    @Bean
+    public RemoteIpFilter remoteIpFilter() {
+        return new RemoteIpFilter();
+    }
+    @Bean
+    public FilterRegistrationBean testFilterRegistration() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new MyFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("MyFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+
+
+
+
+    public class MyFilter implements Filter {
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+
+        }
+
+        @Override
+        public void doFilter(ServletRequest srequest, ServletResponse sresponse, FilterChain filterChain) throws IOException, ServletException {
+
+            HttpServletRequest request = (HttpServletRequest) srequest;
+            System.out.println("this is MyFilter,url :"+request.getRequestURI());
+            filterChain.doFilter(srequest, sresponse);
+
+        }
+
+        @Override
+        public void destroy() {
+
+        }
+    }
+
+
+}
